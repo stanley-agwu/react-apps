7bfeaaa5dc141ff271a4ad07b93f9538c2ff25f6
Stanley Agwu <stanleyagwu2016@gmail.com>
2020-11-09 12:02:50 +0100

    added auth

--- project-mgt-app/src/index.js
+++ project-mgt-app/src/index.js
@@ -7,26 +7,34 @@ import { createStore, applyMiddleware, compose } from 'redux'
 import rootReducer from './store/reducers/rootReducer'
 import { Provider } from 'react-redux'
 import thunk from 'redux-thunk'
-import { reduxFirestore, getFirestore } from 'redux-firestore'
-import { reactReduxFirebase, getFirebase } from 'react-redux-firebase'
-import firebaseConfig from './config/firebaseConfig'
+import { createFirestoreInstance, getFirestore, reduxFirestore } from 'redux-firestore'
+import { ReactReduxFirebaseProvider, getFirebase } from 'react-redux-firebase'
+import firebConfig from './config/firebConfig'
+import firebase from 'firebase/app'
 
-const store = createStore(rootReducer, compose(
-      applyMiddleware(thunk.withExtraArgument({ getFirebase, getFirestore })),
-      reduxFirestore(firebaseConfig),
-      reactReduxFirebase(firebaseConfig)
+  const store = createStore(
+    rootReducer,
+    compose(
+        applyMiddleware(thunk.withExtraArgument({ getFirestore, getFirebase })),
+        reduxFirestore(firebase, firebConfig)
     )
-  )
-
-ReactDOM.render(
-  //<React.StrictMode>
+  );
+  
+  const rrfProps = {
+    firebase,
+    config: firebConfig,
+    dispatch: store.dispatch,
+    createFirestoreInstance
+  };
+  
+  ReactDOM.render(
     <Provider store={store}>
-      <App />
+        <ReactReduxFirebaseProvider {...rrfProps}>
+            <App />
+        </ReactReduxFirebaseProvider>
     </Provider>,
-  //</React.StrictMode>,
-  document.getElementById('root')
-);
-
+    document.getElementById("root")
+  );
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
--- project-mgt-app/src/store/reducers/authReducer.js
+++ project-mgt-app/src/store/reducers/authReducer.js
@@ -1,7 +1,26 @@
-const initState = {}
+const initState = {
+  authError: null
+}
 
 const authReducer = (state = initState, action) => {
-  return state;
+  switch(action.type){
+    case 'LOGIN_ERROR':
+      return {
+        ...state,
+        authError: 'Login failed'
+      }
+      case 'LOGIN_SUCCESS':
+        console.log('Login success')
+        return {
+          ...state,
+          authError: null
+        }
+        case 'SIGNOUT_SUCCESS':
+          console.log('Signout success')
+          return state
+      default:
+        return state
+  }
 };
 
 export default authReducer;
