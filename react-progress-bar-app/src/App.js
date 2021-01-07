20564f0ec94ea67d25f0b13c2aaf7b249a932029
stanleyagwu2016@gmail.com <stanleyagwu2016@gmail.com>
2021-01-07 20:52:03 +0100

    First commit to app

---
+++ react-progress-bar-app/src/App.js
@@ -0,0 +1,24 @@
+import React, { useState, useEffect } from "react";
+import './App.css';
+
+import ProgressBar from "./ProgressBar";
+
+function App() {
+  const [value, setValue] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setValue(oldValue => {
+        const newValue = oldValue + 10;
+
+        newValue === 100 && clearInterval(interval);
+
+        return newValue;
+      });
+    }, 1000);
+  }, []);
+  return (
+    <ProgressBar color={"#F87575"} width={"350px"} value={value} max={100} />
+  );
+}
+export default App;
---
+++ react-progress-bar-app/src/ProgressBar.js
@@ -0,0 +1,62 @@
+import React from "react";
+import PropTypes from "prop-types";
+import Styled from "styled-components";
+
+const Container = Styled.div`
+    display: grid;
+    justify-content: center;
+    align-items: center;
+
+    h1{
+        margin-top: 100px;
+    }
+
+    p{
+        font-weight: 600;
+        font-size: 1.3em;
+    }
+    
+    progress[value] {
+        width: ${props => props.width};
+
+        -webkit-appearance: none;
+        appearance: none;
+    }
+
+    progress[value]::-webkit-progress-bar {
+        height: 20px;
+        border-radius: 20px;
+        background-color: #ccc;
+    }  
+
+    progress[value]::-webkit-progress-value {
+        height: 20px;
+        border-radius: 20px;
+        background-color: ${props => props.color};
+    }
+`;
+
+const ProgressBar = ({ value, max, color, width }) => {
+  return (
+    <Container color={color} width={width}>
+        <h1>Progress Bar</h1>
+        <progress value={value} max={max} />
+        <p>{(value / max) * 100}%</p>
+    </Container>
+  );
+};
+
+ProgressBar.propTypes = {
+  value: PropTypes.number.isRequired,
+  max: PropTypes.number,
+  color: PropTypes.string,
+  width: PropTypes.string
+};
+
+ProgressBar.defaultProps = {
+  max: 100,
+  color: "#CAB6CD",
+  width: "300px"
+};
+
+export default ProgressBar;
