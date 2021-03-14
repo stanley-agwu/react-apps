b2ea155cb1266d2bc007f8ddc0ddc36aa90370f4
stanleyagwu2016@gmail.com <stanleyagwu2016@gmail.com>
2021-03-14 18:47:46 +0100

    updated components

---
+++ movie-rating-app/src/components/AddMovie.js
@@ -0,0 +1,51 @@
+import React, { useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { addMovie } from '../store/actions/movieActions'
+import { nanoid } from '@reduxjs/toolkit'
+
+const AddMovie = () => {
+    const [movie, setMovie] = useState({
+        title: "",
+        director: "",
+        genre: "",
+        rating: ""
+    })
+    const dispatch = useDispatch()
+
+    const handleChange = (e) =>{
+        setMovie({
+            ...movie,
+            [e.target.id]: e.target.value
+        })   
+    }
+    const handleSubmit = (e) =>{
+        e.preventDefault()
+        dispatch(addMovie({ title: movie.title,
+                            director: movie.director,
+                            genre: movie.genre,
+                            rating: movie.rating,
+                            id: nanoid() 
+                })
+        )
+        setMovie({
+            title: "",
+            director: "",
+            genre: "",
+            rating: "5"
+        }) 
+    }
+    return (
+        <div className="add-movie">
+            <h3>Add New Movie</h3>
+            <form onSubmit = {handleSubmit}>
+                <input required type="text" id="title" placeholder="Movie Title" value = {movie.title} onChange = {handleChange}/>
+                <input required type="text" id="director" placeholder="Director" value = {movie.director} onChange = {handleChange}/>
+                <input required type="text" id="genre" placeholder="Genre" value = {movie.genre} onChange = {handleChange}/>
+                <input required type="number" id="rating" placeholder="Rating(1 - 10)" value = {movie.rating} onChange = {handleChange} s/>
+                <button>Submit</button>
+            </form>
+        </div>
+    )
+            
+}
+export default AddMovie
--- movie-rating-app/src/store/store.js
+++ movie-rating-app/src/store/store.js
@@ -1,5 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit'
-import movieReducer from './reducers/movieReducers'
+import movieReducer from '../../../../src/store/reducers/movieReducers'
 
 const store = configureStore({
     reducer: { movies: movieReducer }
