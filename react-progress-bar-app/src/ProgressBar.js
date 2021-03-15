5501593cb683720ce5080f8d26654327107a2c6a
stanleyagwu2016@gmail.com <stanleyagwu2016@gmail.com>
2021-03-15 20:24:52 +0100

    updated progress styles
    updated progress styles

--- react-progress-bar-app/src/ProgressBar.js
+++ react-progress-bar-app/src/ProgressBar.js
@@ -3,42 +3,54 @@ import PropTypes from "prop-types";
 import Styled from "styled-components";
 
 const Container = Styled.div`
-    display: grid;
+    display: flex;
     justify-content: center;
     align-items: center;
-    height: 100vh;
+    flex-direction: column;
+    height: 80vh;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+    'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+    sans-serif;
 
     p{
         font-weight: 600;
-        font-size: 1.3em;
+        font-size: 1.6em;
     }
     
     progress[value] {
-        width: ${props => props.width};
-
+        width: ${({width}) => width || "200px"};
         -webkit-appearance: none;
         appearance: none;
     }
 
     progress[value]::-webkit-progress-bar {
-        height: 20px;
+        height: 40px;
         border-radius: 20px;
         background-color: #ccc;
     }  
 
     progress[value]::-webkit-progress-value {
-        height: 20px;
+        height: 40px;
         border-radius: 20px;
-        background-color: ${props => props.color};
+        background-color: ${({color}) => color};
+    }
+    div{
+      display: inline-flex;
+    }
+    p{
+      margin-left: 20px;
+      transform: translateY(-80%)
     }
 `;
 
 const ProgressBar = ({ value, max, color, width }) => {
   return (
     <Container color={color} width={width}>
-        <span><h1>Progress Bar</h1>
-        <progress value={value} max={max} />
-        <p>{(value / max) * 100}%</p></span>
+        <h1>Progress Bar</h1>
+        <div>
+          <progress value={value} max={max} width={width} color={color}/>
+          <p>{(value / max) * 100}%</p>
+        </div>
     </Container>
   );
 };
@@ -53,7 +65,7 @@ ProgressBar.propTypes = {
 ProgressBar.defaultProps = {
   max: 100,
   color: "#CAB6CD",
-  width: "300px"
+  width: "400px"
 };
 
 export default ProgressBar;
