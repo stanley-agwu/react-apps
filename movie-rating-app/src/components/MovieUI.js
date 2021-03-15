3a867c01c17bd9281f676f9afd202c9cd7145152
stanleyagwu2016@gmail.com <stanleyagwu2016@gmail.com>
2021-03-15 04:11:16 +0100

    updated components

---
+++ movie-rating-app/src/components/MovieUI.js
@@ -0,0 +1,31 @@
+import React from "react"
+import { useSelector, useDispatch } from 'react-redux'
+import { deleteMovie } from '../store/actions/movieActions'
+
+const MoviesUI = () =>{
+    const movies = useSelector(state => state.movies.movies)
+    const dispatch = useDispatch()
+    const handleDelete = (id) => {
+        dispatch(deleteMovie({ id }))
+    }
+    let movieList
+    movies.length ? (
+        movieList = movies.map(movie => { 
+            return (
+                    <div className="movie" key={movie.id}>
+                        <h3>{movie.title}</h3>
+                        <span onClick={() => handleDelete(movie.id)}><i className="far fa-trash-alt"></i></span>
+                        <p>{movie.director}</p>
+                        <p>{movie.genre}</p>
+                    </div>
+                    )
+                })
+        ) : movieList = <p className="empty">There are no movies to display!</p>
+    
+    return (
+        <div className="movies-list">
+            {movieList}
+        </div>
+    )
+}
+export default MoviesUI 
