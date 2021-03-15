c1b3f7bc3c797fa8304027d07ef038cb04973f07
stanleyagwu2016@gmail.com <stanleyagwu2016@gmail.com>
2021-03-15 04:14:21 +0100

    added changes

--- movie-rating-app/src/App.js
+++ movie-rating-app/src/App.js
@@ -1,8 +1,17 @@
+import Header from './components/Header'
+import AddMovie from './components/AddMovie';
+import MoviesUI  from './components/MovieUI'
+
 const App = () =>{
   return (
     <div className="App">
-      <h1>Movie Rating App</h1>
+      <Header />
+      <div className="inner-App">
+        <MoviesUI />
+        <AddMovie />
       </div>
+             
+    </div>
   )
 }
 
