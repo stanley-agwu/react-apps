23b42b304e87cff03cd74396ffd7abc0ea914708
stanleyagwu2016@gmail.com <stanleyagwu2016@gmail.com>
2021-03-15 22:17:35 +0100

    added changes

--- react-progress-tracker/src/App.js
+++ react-progress-tracker/src/App.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const Title = styled.h1`
+  text-align: center;
+  color: #E6AACE;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+    'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+    sans-serif;
+    margin: 20px;
+    padding: 0;
+`
 function App() {
   return (
     <div className="App">
+      
       <div className="Main">
-        <h1>Daily Progress Tracker</h1>
+        <Title>Daily Progress Tracker</Title>
         <div className="Section">
           <div className="Weekday-Title">M</div>
         </div>
---
+++ react-progress-tracker/src/components/section.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Styled from 'styled-components'
+
+const StyledSection = styled.div`
+    background-color: #f2f2f2;
+    width: 350px;
+    height: 50px;
+    border-radius: 10px;
+    border: 3px solid grey;
+    margin: 10px;
+`
+
+const Section = () => {
+    return (
+        <StyledSection>
+            
+        </StyledSection>
+
+    )
+}
