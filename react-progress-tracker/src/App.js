a5980ac2d1f8cf553fc6ef9c5258dc2e0bfa1fe7
stanleyagwu2016@gmail.com <stanleyagwu2016@gmail.com>
2021-03-16 18:48:27 +0100

    added styled-components styles

--- react-progress-tracker/src/App.js
+++ react-progress-tracker/src/App.js
@@ -4,40 +4,71 @@ import styled from 'styled-components'
 const Title = styled.h1`
   text-align: center;
   color: #E6AACE;
-  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
-    'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
-    sans-serif;
-    margin: 20px;
-    padding: 0;
+  font-family: sans-serif, 'Segoe UI', 'Roboto', 'Oxygen';
+  margin: 20px;
+  padding: 0;
+`
+const Section = styled.div`
+  background-color: #f2f2f2;
+  width: 350px;
+  height: 50px;
+  border-radius: 10px;
+  border: 3px solid grey;
+  margin: 10px;
+`
+
+const Main = styled.div`
+  font-family: sans-serif, 'Segoe UI', 'Roboto', 'Oxygen';
+  width: 600px;
+  border-radius: 20px;
+  margin: 0 auto;
+  padding: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 90vh;
+  flex-direction: column;
+  border: 1px solid grey;
+`
+
+const WeekdayTitle = styled.div`
+  width: 50px;
+  height: 100%;
+  border-right: 3px solid lightgrey;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 2.5rem;
+  color: #ff6961;
 `
 function App() {
   return (
     <div className="App">
       
-      <div className="Main">
+      <Main>
         <Title>Daily Progress Tracker</Title>
-        <div className="Section">
-          <div className="Weekday-Title">M</div>
-        </div>
-        <div className="Section">
-          <div className="Weekday-Title">T</div>
-        </div>
-        <div className="Section">
-          <div className="Weekday-Title">W</div>
-        </div>
-        <div className="Section">
-          <div className="Weekday-Title">T</div>
-        </div>
-        <div className="Section">
-          <div className="Weekday-Title">F</div>
-        </div>
-        <div className="Section">
-          <div className="Weekday-Title">S</div>
-        </div>
-        <div className="Section">
-          <div className="Weekday-Title">S</div>
-        </div>
-      </div>
+        <Section>
+          <WeekdayTitle>M</WeekdayTitle>
+        </Section>
+        <Section>
+          <WeekdayTitle>T</WeekdayTitle>
+        </Section>
+        <Section>
+          <WeekdayTitle>W</WeekdayTitle>
+        </Section>
+        <Section>
+          <WeekdayTitle>T</WeekdayTitle>
+        </Section>
+        <Section>
+          <WeekdayTitle>F</WeekdayTitle>
+        </Section>
+        <Section>
+          <WeekdayTitle>S</WeekdayTitle>
+        </Section>
+        <Section>
+          <WeekdayTitle>S</WeekdayTitle>
+        </Section>
+      </Main>
     </div>
   );
 }
