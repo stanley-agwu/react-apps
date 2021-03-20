ebf7a0ae9cc62c9449cbee0b7344a471c5c7eb9f
stanleyagwu2016@gmail.com <stanleyagwu2016@gmail.com>
2021-03-20 06:58:27 +0100

    added changes

--- movie-search-app/src/components/SearchMovies.js
+++ movie-search-app/src/components/SearchMovies.js
@@ -1,8 +1,9 @@
 const SearchMovies = () => {
     return (
         <form className="form">
-            <label className="label" htmlFor="query" >Movie Name: </label>
+            <label className="label" htmlFor="query" >Movie Name</label>
             <input className="input" name="query" placeholder="i.e. Jurassic Park" />
+            <button className="button">Search</button>
         </form>
     )
 }
