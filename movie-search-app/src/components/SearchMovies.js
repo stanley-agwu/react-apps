cfbe48518a6704cf5f165be9a7a27393316630d1
stanleyagwu2016@gmail.com <stanleyagwu2016@gmail.com>
2021-03-20 10:14:19 +0100

    search function

--- movie-search-app/src/components/SearchMovies.js
+++ movie-search-app/src/components/SearchMovies.js
@@ -1,6 +1,24 @@
 const SearchMovies = () => {
+    const searchMovies = async (e) => {
+        e.preventDefault()
+
+
+        const queryString = "Jurassic Park"
+        const API_KEY = API_KEY_String
+
+        const url = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&
+                        language=en-US&query=${queryString}&page=1&include_adult=false`
+        try {
+            const response = await fetch(url)
+            const data = await response.json()
+            console.log(data)   
+        } catch (error) {
+            console.error(error)
+
+        }
+    }
     return (
-        <form className="form">
+        <form className="form" onSubmit={searchMovies}>
             <label className="label" htmlFor="query" >Movie Name</label>
             <input className="input" name="query" placeholder="i.e. Jurassic Park" />
             <button className="button">Search</button>
